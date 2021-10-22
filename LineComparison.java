3920216b95c7619327d8deb2f71af09f3492fa46
pranay <pranaykawale500@gmail.com>
2021-10-22 13:52:45 +0530

    Added: CompareTo and Equal method used using Oops concept

--- LineComparison.java
+++ LineComparison.java
@@ -16,8 +16,13 @@ public class LineComparison {
 		value.setLength2();
 		System.out.println("Length of line 1 ::" + value.getLength1());
 		System.out.println("Length of line 2 ::"+ value.getLength2());
-		double a = value.getLength1() ;
-		double b = value.getLength2() ;
+		double a = value.getLength1();
+		double b = value.getLength2();
+		value.compareLines(a,b);
+		value.equalLines(a, b);
+	}	
+	void compareLines(double a, double b) {
+
 		if (Double.compare(a, b) == 0) {
 			System.out.println("Length1 = Length2");
 		}
@@ -27,7 +32,16 @@ public class LineComparison {
 		else {
 			System.out.println("Length1 is greater than Length2");
 		}
+
 	}
+
+	void equalLines(double a , double b) {
+		String str1 = Double.toString(a);
+		String str2 = Double.toString(b);
+		System.out.println("Is length of line 1 = length of line 2  ");
+		System.out.println(str1.equals(str2));
+	}
+
 	public void enterValues1() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter x1: ");
