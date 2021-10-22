844db2a442f6274310f7d933b5df6b43a796ce66
pranay <pranaykawale500@gmail.com>
2021-10-22 12:57:09 +0530

    Added: Checking equality of two Lines

--- LineComparison.java
+++ LineComparison.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 
 public class LineComparison {
 
-	static int x1, x2,  y1, y2;
-	static double length;
+	static int x1, x2, x3, x4,  y1, y2 , y3 ,y4;
+	static double length1 , Length2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation program");
 		LineComparison value = new LineComparison();
-		value.enterValues();
-		value.setLength();
-		double length = value.getLength();
-		System.out.println("The length of line :: " + length);
-
+		value.enterValues1();
+		value.enterValues2();
+		value.setLength1();
+		value.setLength2();
+		System.out.println("Length of line 1 ::" + value.getLength1());
+		System.out.println("Length of line 2 ::"+ value.getLength2());
+		String str1 = Double.toString(value.getLength1());
+		String str2 = Double.toString(value.getLength2());
+		System.out.println("Is length of line 1 = length of line 2  ");
+		System.out.println(str1.equals(str2));
 	}
-	public void enterValues() {
+	public void enterValues1() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter x1: ");
 		x1 = sc.nextInt();
@@ -29,13 +34,30 @@ public class LineComparison {
 		y2 = sc.nextInt();
 	}
 
+	public void enterValues2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter x3: ");
+		x3 = sc.nextInt();
+		System.out.print("Enter x4: ");
+		x4 = sc.nextInt();
 
-	public void setLength(){
-		length = Math.sqrt(((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+		System.out.print("Enter y3: ");
+		y3 = sc.nextInt();
+		System.out.print("Enter y4: ");
+		y4 = sc.nextInt();
 	}
-	public double getLength(){
-		return length;
+
+	public void setLength1(){
+		length1 = Math.sqrt(((x2 - x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
 	}
 
-
+	public double getLength1(){
+		return length1;
+	}
+	public void setLength2(){
+		Length2 = Math.sqrt(((x4 - x3) * (x4-x3)) + ((y4-y3) * (y4-y3)));
+	}
+	public double getLength2(){
+		return Length2;
+	}
 }
